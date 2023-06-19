6a6965fdef5434b2e10edf9a87a2c291ef998555
Matias Luis Yacob <matiasyacob@hotmail.com>
2023-06-19 12:10:10 -0300

    me quiero morir

--- Js/MainJS.js
+++ Js/MainJS.js
@@ -25,8 +25,7 @@ let carrito = [];
 // El "contenidoCard" lo voy a estar usando para mostrar en pantalla tanto los Artículos como el carrito
 let contenidoCard = "";
 
-
-//Funcion de Render principal que muestra los Articulos utilizando un innerHTML (uso de evento de usuario "onClick" y DOM)
+// Función de render principal que muestra los Artículos utilizando un innerHTML (uso de evento de usuario "onClick" y DOM)
 function renderMain() {
     productos.forEach(articulo => {
         contenidoCard += `
@@ -38,10 +37,11 @@ function renderMain() {
                 <p><button class="btn btn-primary" onClick="agregarAlCarrito('${articulo.nombre}');">Agregar Al Carro</button></p>
             </div>
         </div>
-    `;
+        `;
     });
 }
-//Funcion para Agregar Articulos al Carrito (uso de Local Storage y JSON)
+
+// Función para Agregar Artículos al Carrito (uso de Local Storage y JSON)
 function agregarAlCarrito(nombre) {
     let producto = productos.find(item => item.nombre === nombre);
 
@@ -51,50 +51,62 @@ function agregarAlCarrito(nombre) {
         console.log("Producto agregado al carrito:", producto.nombre);
     }
 }
-//Funcion para mostrar en pantalla una lista con los Articulos cargados al carrito.
+
+// Función para mostrar en pantalla una lista con los Artículos cargados al carrito.
+
 function renderCarrito() {
     let contenidoCard = "";
+    //esto es porara que verifique si hay datos cargados en local storage.
+    const carritoLocalStorage = localStorage.getItem("Carrito");
+    if (carritoLocalStorage) {
+        carrito = JSON.parse(carritoLocalStorage);
+
+    };
+    //aca se crea el contenido que se muestra en pantalla por cada articulo agregado al carrito
     carrito.forEach(articulo => {
         contenidoCard += `
-            <div class="alert alert-success text-center" role="alert"> <p>${articulo.nombre}</p><p><b>$${articulo.precio}</b></p>  
+            <div class="alert alert-success text-center" role="alert">
+                <p>${articulo.nombre}</p>
+                <p><b>$${articulo.precio}</b></p>  
             </div>
         `;
     });
-   
-
+    //se ejecuta junto con la funcion que calcula el monto total de los articulos a pagar
     let totalCarrito = calcularTotalCarrito();
     console.log("Total del carrito:", totalCarrito);
+
     // Asigno el contenido del Card para mostrarlo en pantalla, en este caso con el contenido del Carrito.
     document.getElementById("contenido").innerHTML = contenidoCard;
-    
+
     // Quitar la clase "container" del div "contenido" esto es para que deje de tener "Display: flex;"" y se agrupen en columna.
     let div = document.getElementById("contenido");
     div.classList.remove("container");
-    
-    // Mostrar el total del carrito y le quito la class "displayNone" para que ahora si se muestre (estaba oculto)
+
+    // Mostrar el total del carrito y le quito la class "displayNone" para que ahora sí se muestre (estaba oculto)
     let divTotal = document.getElementById("total");
     divTotal.classList.remove("displayNone");
     document.getElementById("total").innerHTML = `<p>Total a Pagar: $${totalCarrito}</p>`;
 }
 
-
 // Ejecuto la funcion del Render Principal
 renderMain();
 
-//Asigno el contenido del Card para mostrarlo en pantalla, en este caso la lista de articulos que se pueden agregar al carrito.
+// Asigno el contenido del Card para mostrarlo en pantalla, en este caso la lista de artículos que se pueden agregar al carrito.
 document.getElementById("contenido").innerHTML = contenidoCard;
 
-//Funcion que borra el local storage, vacia el array de carrito y recarga la pagina.
+// Funcion que borra el Local Storage, vacía el array de carrito y recarga la página.
 function borrarCarrito() {
     localStorage.clear();
     carrito = [];
     recargarPagina();
 }
-//funcion para recargar la pagina.
+
+// Funcion para recargar la página.
 function recargarPagina() {
     location.reload();
 }
-//funcion del calculo para sumar el total del carrito.
+
+// Funcion del cálculo para sumar el total del carrito.
 function calcularTotalCarrito() {
     let total = carrito.reduce((acumulador, articulo) => {
         return acumulador + articulo.precio;
